c45ea0a6d17ea231ca4ac5702d1a2179a1ff705e
xsavzh <drockick@gmail.com>
2023-01-17 21:32:12 +0400

    Update code

--- app/src/main/java/com/xsavzh/musicplayer/MainActivity.kt
+++ app/src/main/java/com/xsavzh/musicplayer/MainActivity.kt
@@ -1,15 +1,53 @@
 package com.xsavzh.musicplayer
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.xsavzh.musicplayer.databinding.ActivityMainBinding
+import java.util.*
+import kotlin.concurrent.timerTask
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private var mediaPlayer = MediaPlayer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        mediaPlayer = MediaPlayer.create(applicationContext, R.raw.burzum)
+
+        seekbar()
+        buttonActions()
     }
+
+    private fun seekbar()  = with(binding) {
+        seekbar.max = mediaPlayer.duration
+
+        Timer().scheduleAtFixedRate(timerTask {
+            seekbar.progress = mediaPlayer.currentPosition
+        }, 0, 1)
+    }
+
+     private fun buttonActions() = with(binding) {
+         skipPreviousImageButton.setOnClickListener {
+
+         }
+
+         skipNextImageButton.setOnClickListener {
+
+         }
+
+         playImageButton.setOnClickListener {
+             if (mediaPlayer.isPlaying) {
+                 mediaPlayer.pause()
+                 playImageButton.setImageResource(R.drawable.ic_baseline_play_arrow_75)
+             } else {
+                 mediaPlayer.start()
+                 playImageButton.setImageResource(R.drawable.ic_baseline_pause_75)
+             }
+         }
+     }
 }
