278bda05526170be3e5ffefea3359d1e599824d3
xsavzh <drockick@gmail.com>
2023-01-26 23:10:56 +0400

    Update code

--- app/src/main/java/com/xsavzh/musicplayer/MainActivity.kt
+++ app/src/main/java/com/xsavzh/musicplayer/MainActivity.kt
@@ -10,18 +10,35 @@ import kotlin.concurrent.timerTask
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    private var index: Int = 0
+
     private var mediaPlayer = MediaPlayer()
+
+    private val trackList = listOf(
+        R.raw.feeblescreamsfromforestsunknown,
+        R.raw.ealordofthedeeps,
+        R.raw.blackspellofdestruction,
+        R.raw.channellingthepowerofsoulsintoanewgod,
+        R.raw.war,
+        R.raw.thecryingorc,
+        R.raw.myjourneytothestars,
+        R.raw.dungeonsofdarkness,
+        R.raw.stemmenfrataarnet,
+        R.raw.dominussathanas,
+        R.raw.alostforgottensadspirit
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        mediaPlayer = MediaPlayer.create(applicationContext, R.raw.burzum)
+        mediaPlayer = MediaPlayer.create(applicationContext, trackList[index])
 
         seekbar()
         buttonActions()
     }
+
 
     private fun seekbar()  = with(binding) {
         seekbar.max = mediaPlayer.duration
@@ -33,11 +50,21 @@ class MainActivity : AppCompatActivity() {
 
      private fun buttonActions() = with(binding) {
          skipPreviousImageButton.setOnClickListener {
+            mediaPlayer.pause()
+            index--
+            if (index < 0) index = trackList.size - 1
 
+            mediaPlayer = MediaPlayer.create(applicationContext, trackList[index])
+            mediaPlayer.start()
          }
 
          skipNextImageButton.setOnClickListener {
+             mediaPlayer.pause()
+             index++
+             if (index > trackList.size - 1) index = 0
 
+             mediaPlayer = MediaPlayer.create(applicationContext, trackList[index])
+             mediaPlayer.start()
          }
 
          playImageButton.setOnClickListener {
